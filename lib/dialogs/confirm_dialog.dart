a945ddca25b6eaa2b0c0befadce4d42025175c31
AgustinaGF <agustinagomez385@gmail.com>
2023-07-27 12:54:27 -0300

    157

--- lib/dialogs/confirm_dialog.dart
+++ lib/dialogs/confirm_dialog.dart
@@ -6,23 +6,56 @@ Future<bool> showConfirmDialog(
 }) async {
   final result = await showDialog<bool>(
     context: context,
-    builder: (context) => AlertDialog(
-      title: Text(title),
+    barrierColor: Colors.white.withOpacity(0.8),
+    barrierDismissible: false,
+    builder: (context) => _DialogContent(
+      title: title,
+    ),
+  );
+  return result ?? false;
+}
+
+class _DialogContent extends StatelessWidget {
+  const _DialogContent({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+        15,
+      )),
+      actionsAlignment: MainAxisAlignment.center,
       actions: [
         MaterialButton(
           onPressed: () {
             Navigator.pop(context, true);
           },
-          child: const Text('Yes'),
+          child: const Text(
+            'Yes',
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
         ),
         MaterialButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: const Text('No'),
+          child: const Text(
+            'No',
+            style: TextStyle(
+              color: Colors.redAccent,
+            ),
+          ),
         ),
       ],
-    ),
-  );
-  return result ?? false;
+    );
+  }
 }
