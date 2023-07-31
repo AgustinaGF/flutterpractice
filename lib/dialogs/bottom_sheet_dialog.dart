4d00d4f635a5b08100c07575a1461069330002bb
AgustinaGF <agustinagomez385@gmail.com>
2023-07-31 12:11:00 -0300

    160

--- lib/dialogs/bottom_sheet_dialog.dart
+++ lib/dialogs/bottom_sheet_dialog.dart
@@ -3,34 +3,47 @@ import 'package:flutter/material.dart';
 Future<void> ShowBottomSheetDialog(BuildContext context) {
   return showModalBottomSheet(
     context: context,
+    backgroundColor: Colors.transparent,
     //esto evita el error de overflow
     isScrollControlled: true,
-    builder: (context) => SafeArea(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListTile(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            leading: const Icon(Icons.photo),
-            title: const Text('Photos'),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            leading: const Icon(Icons.music_note),
-            title: const Text('Music'),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            leading: const Icon(Icons.video_camera_back_rounded),
-            title: const Text('Video'),
-          )
-        ],
+    builder: (context) => Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.elliptical(20, 10),
+          topRight: Radius.elliptical(20, 10),
+        ),
+      ),
+      constraints: BoxConstraints(
+        maxWidth: 480,
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              leading: const Icon(Icons.photo),
+              title: const Text('Photos'),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              leading: const Icon(Icons.music_note),
+              title: const Text('Music'),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              leading: const Icon(Icons.video_camera_back_rounded),
+              title: const Text('Video'),
+            )
+          ],
+        ),
       ),
     ),
   );
